df85f20b8d339a311309242f224236ec16883f7d
HanYooJa <hyj03250@naver.com>
2022-11-25 16:44:44 +0900

    3

--- components/Layout.js
+++ components/Layout.js
@@ -1,33 +1,45 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useContext } from 'react'
+import { Store } from '../utils/Store'
 
 export default function Layout({ title, children }) {
-  return <>
-    <Head>
-      <title>{title ? title + '-Book-site' : 'Book-site'}</title>
-      <meta name="description" content="Book-site" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <div className="flex min-h-screen flex-col justify-between">
-      <header>
-        <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
-          <Link href="/" className="text-lg font-bold">
-            Book-site
-          </Link>
-          <div>
-            <Link href="/cart" className="p-2">
-              Cart
+  const { state } = useContext(Store)
+  const { cart } = state
+
+  return (
+    <>
+      <Head>
+        <title>{title ? title + '-Book-site' : 'Book-site'}</title>
+        <meta name="description" content="Book-site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
+            <Link href="/" className="text-lg font-bold">
+              Book-site
             </Link>
-            <Link href="/login" className="p-2">
-              Login
-            </Link>
-          </div>
-        </nav>
-      </header>
-      <main className="container m-auto mt-4 px-4">{children}</main>
-      <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
-        <p>Copyright &copy; 2022 Book-site</p>
-      </footer>
-    </div>
-  </>;
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </span>
+                )}
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
+          <p>Copyright &copy; 2022 Book-site</p>
+        </footer>
+      </div>
+    </>
+  )
 }
