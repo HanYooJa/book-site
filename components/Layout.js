9382140fa602f9f6824f65636ad0f246e47ff306
HanYooJa <hyj03250@naver.com>
2022-11-29 15:58:41 +0900

    9

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { useContext, useEffect, useState } from 'react'
 import { Store } from '../utils/Store'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useSession } from 'next-auth/react'
 
 export default function Layout({ title, children }) {
   const { state } = useContext(Store)
@@ -18,6 +21,9 @@ export default function Layout({ title, children }) {
         <meta name="description" content="Nextjs Ecommerce" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
+
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
@@ -35,6 +41,16 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
+
               <Link href="/login" className="p-2">
                 Login
               </Link>
